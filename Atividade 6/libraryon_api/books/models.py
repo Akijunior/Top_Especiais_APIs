d9852006f4f126dfa15ff46817e5efa30d83c77d
Akijunior <suitsu19@gmail.com>
2018-06-22 17:01:13 -0300

    Criação dos models de usuário e utilitários.

--- Atividade 6/libraryon_api/books/models.py
+++ Atividade 6/libraryon_api/books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     edition = models.CharField(max_length=20)
     year = models.IntegerField()
     amount_pages = models.IntegerField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     authors = models.ManyToManyField('users.Author')
     genres = models.ManyToManyField('Genre')
 
@@ -18,12 +18,19 @@ class Score(models.Model):
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     lector = models.ForeignKey('users.Lector', on_delete=models.CASCADE)
     score = models.DecimalField(decimal_places=2, validators=[
-            MaxValueValidator(10.0), MinValueValidator(0.0)])
+            MaxValueValidator(10.0), MinValueValidator(0.0)], max_digits=4)
     comment = models.CharField(max_length=200, blank=True)
     evaluation_date = models.DateTimeField(auto_now_add=True)
     last_update_date = models.DateTimeField(auto_now=True)
 
 
 class Genre(models.Model):
+    AGE_RANGE = (
+        ('F', 'Free'),
+        ('FT', '+14'),
+        ('ST', '+16'),
+        ('ET', '+18'),
+    )
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
+    age_range = models.CharField(max_length=2, choices=AGE_RANGE, default='F')
--- Atividade 6/libraryon_api/users/models.py
+++ Atividade 6/libraryon_api/users/models.py
@@ -5,8 +5,10 @@ class Author(models.Model):
     name = models.CharField(max_length=150)
     age = models.IntegerField()
     books = models.ManyToManyField('books.Book')
+    auth_profile = models.OneToOneField('auth.User', on_delete=models.CASCADE)
 
 
 class Lector(models.Model):
     name = models.CharField(max_length=150)
     age = models.IntegerField()
+    lector_profile = models.OneToOneField('auth.User', on_delete=models.CASCADE)
