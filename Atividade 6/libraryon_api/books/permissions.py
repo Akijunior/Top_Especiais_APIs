2988b0b9d4e2c31e11e5565fe2be1c3da0421e5b
Akijunior <suitsu19@gmail.com>
2018-06-25 15:53:08 -0300

    Adição de permissões ao app books.

--- Atividade 6/libraryon_api/books/permissions.py
+++ Atividade 6/libraryon_api/books/permissions.py
@@ -1,8 +1,41 @@
 from rest_framework import permissions
 
-from .models import *
+from users.models import Author
 
 
+class OnlyAuthorCanCreateABook(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return Author.objects.get(auth_profile=request.user).exists()
 
 
+class IsAuthorOfBookOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            if Author.objects.get(auth_profile=request.user).exists():
+                author = Author.objects.get(auth_profile=request.user)
+                return obj.authors.filter(auth_profile=author.auth_profile).exists()
+            return False
 
+
+class OnlyTheLectorWhoAssignedTheScoreCanEditIt(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj.lector == request.user
+
+
+# class IsOwnerOfCommentOrManagerOfPost(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         else:
+#             id = obj.postId
+#             post = Post.objects.get(id=id.id)
+#             user = User.objects.get(id=post.userId.id)
+#             return request.user in user.profiles.all()
--- Atividade 6/libraryon_api/books/views.py
+++ Atividade 6/libraryon_api/books/views.py
@@ -1,14 +1,6 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import renderers
-from rest_framework import viewsets, generics, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
+from rest_framework import generics
 
-from rest_framework.authtoken.models import Token
-from rest_framework.reverse import reverse
-from rest_framework.throttling import ScopedRateThrottle
-
-from users.permissions import *
+from books.permissions import *
 from .serializers import *
 
 
@@ -16,40 +8,43 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     name = 'book-list'
-    permission_classes = (permissions.IsAuthenticated, ReadUserOnly, )
+    permission_classes = (permissions.IsAuthenticated, OnlyAuthorCanCreateABook,)
+
+    def perform_create(self, serializer):
+        author = Author.objects.get(auth_profile=self.request.user)
+        authors = []
+        authors.append(author)
+        serializer.save(authors=authors)
 
 
 class BookDetail(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     name = 'book-detail'
-    permission_classes = (permissions.IsAuthenticated, ReadUserOnly, )
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOfBookOrReadOnly,)
 
 
 class GenreList(generics.ListAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     name = 'genre-list'
-    permission_classes = (permissions.IsAuthenticated, ReadUserOnly, )
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
 
 
 class GenreDetail(generics.RetrieveAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     name = 'genre-detail'
-    permission_classes = (permissions.IsAuthenticated, ReadUserOnly, )
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
 
 
 class ScoreList(generics.ListCreateAPIView):
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
     name = 'score-list'
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
-        # score = Score.objects.all().get(lector=self.request.user, book=self.request)
-        if Score.objects.all().filter(lector=self.request.user, book=self.request.get('book', None)).exists():
-            Score.objects.all().filter(lector=self.request.user, book=self.kwargs.get('book', None)).delete()
         serializer.save(lector=self.request.user)
 
 
@@ -57,4 +52,4 @@ class ScoreDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
     name = 'score-detail'
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated, OnlyTheLectorWhoAssignedTheScoreCanEditIt,)
